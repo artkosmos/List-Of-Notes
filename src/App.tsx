a19f7e2623dd34e8869057205fb50863e04f46cb
Artem Kosmatkov <artemkosmatkov1994@gmail.com>
2023-05-08 21:00:27 +0300

    feat: implement input, add task and refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import './App.css';
 import ToDoList, {TaskType} from "./ToDoList";
+import {v1} from "uuid";
 
 export type FilterType = 'all' | 'active' | 'completed'
 
@@ -8,10 +9,11 @@ function App() {
   // we store data higher to follow "raise the state" concept
   const title: string = "What to learn?"
   const [tasksData, setTasksData] = useState<TaskType[]>([ // always return array including 2 elements(arr,fnc)
-    {id: 1, title: "HTML&CSS", isChecked: true},
-    {id: 2, title: "JS/TS/ES6", isChecked: true},
-    {id: 3, title: "React", isChecked: false},
-    {id: 4, title: "Angular", isChecked: false}
+    // v1 sets unique id number
+    {id: v1(), title: "HTML&CSS", isChecked: true},
+    {id: v1(), title: "JS/TS/ES6", isChecked: true},
+    {id: v1(), title: "React", isChecked: false},
+    {id: v1(), title: "Angular", isChecked: false}
   ])
   const [filter, setFilter] = useState<FilterType>("all")
 
@@ -19,10 +21,16 @@ function App() {
     setFilter(filter)
   }
 
-  const removeTask = (taskId: number): void => {
+  const removeTask = (taskId: string): void => {
     const updatedTasksData = tasksData.filter((item) => item.id !== taskId)
     setTasksData(updatedTasksData)
     // return updated tasks array without removed(x) value in main state
+  }
+
+  const addTask = (text: string) => {
+    const newTask = {id: v1(), title: text, isChecked: false}
+    const newTasks = [newTask, ...tasksData]
+    setTasksData(newTasks)
   }
 
   const getFilteredTask = (tasksData: TaskType[], filter: FilterType): TaskType[] => {
@@ -37,6 +45,8 @@ function App() {
   }
   const filteredTasksData = getFilteredTask(tasksData, filter)
 
+  // Main concept:
+  // logic function --> some actions + set function --> logic function throw through the props
   return (
     <div className="App">
       <ToDoList
@@ -44,6 +54,7 @@ function App() {
         title={title}
         removeTask={removeTask}
         changeFilter={changeFilter}
+        addTask={addTask}
       />
     </div>
   )
--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,46 +1,89 @@
-import React from 'react';
+import React, {useState, KeyboardEvent, ChangeEvent} from 'react';
 import {FilterType} from "./App";
 
 type ToDoListPropsType = {
   title: string
   tasksData: TaskType[]
-  removeTask: (taskId: number) => void
+  removeTask: (taskId: string) => void
   changeFilter: (filter: FilterType) => void
+  addTask: (text: string) => void
 }
 
 export type TaskType = {
-  id: number
+  id: string
   title: string
   isChecked: boolean
 }
 
-const ToDoList: React.FC<ToDoListPropsType> = ({tasksData, title, removeTask, changeFilter}) => {
-  // const  {tasks, title} = props --> the write is longer than above in brackets
+const ToDoList: React.FC<ToDoListPropsType> = ({tasksData, title, removeTask, changeFilter, addTask}) => {
+  // const  {tasks, title} = props --> the write is the same, but it'll be longer than above in brackets
+
+  let [text, setText] = useState('')
+
+  const anotherTask = () => {
+    addTask(text)
+    setText('')
+  }
+
+  const onPressHandler = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      anotherTask()
+    }
+  }
+
+  const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    const currentValue = event.currentTarget.value
+    setText(currentValue)
+  }
+
+  const onClickHandlerAll = () => {
+    changeFilter("all")
+  }
+
+  const onClickHandlerActive = () => {
+    changeFilter("active")
+  }
+
+  const onClickHandlerCompleted = () => {
+    changeFilter("completed")
+  }
+
   const tasksJSX: JSX.Element[] = tasksData.map((item) => {
+
+    const onClickRemoveHandler = () => {
+      removeTask(item.id)
+    }
+
     return (
-      // key is the difference between old and new added element for React
+      // key need to add always or may be error
       <li key={item.id}>
         <input type="checkbox" checked={item.isChecked}/>
         <span>{item.title}</span>
-        <button onClick={() => removeTask(item.id)}>x</button>
+        <button onClick={onClickRemoveHandler}>x</button>
       </li>
     )
   })
+
+  // render
   return (
     <div>
       <div>
         <h3>{title}</h3>
         <div>
-          <input/>
-          <button>+</button>
+          <input
+            value={text}
+            onChange={onChangeHandler}
+            onKeyDown={onPressHandler}
+          />
+          <button onClick={anotherTask}>+</button>
         </div>
         <ul>
           {tasksJSX}
         </ul>
         <div>
-          <button onClick={() => changeFilter("all")}>All</button>
-          <button onClick={() => changeFilter("active")}>Active</button>
-          <button onClick={() => changeFilter("completed")}>Completed</button>
+          <button onClick={onClickHandlerAll}>All</button>
+          <button onClick={onClickHandlerActive}>Active</button>
+          <button onClick={onClickHandlerCompleted}>Completed</button>
         </div>
       </div>
     </div>
