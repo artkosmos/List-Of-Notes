9625738e70c26ec02c2e8504915f9ade4b0f090a
Artem Kosmatkov <artemkosmatkov1994@gmail.com>
2023-05-16 16:35:30 +0300

    feat: implement wrong input error

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,5 +1,6 @@
 import React, {useState, KeyboardEvent, ChangeEvent} from 'react';
 import {FilterType} from "./App";
+import style from './ToDoList.module.css'
 
 type ToDoListPropsType = {
   title: string
@@ -26,11 +27,16 @@ const ToDoList: React.FC<ToDoListPropsType> = ({
                                                }) => {
   // const  {tasks, title} = props --> the write is the same, but it'll be longer than above in brackets
 
-  let [text, setText] = useState('')
+  const [text, setText] = useState('')
+  const [error, setError] = useState<string | null>('')
 
   const anotherTask = () => {
-    addTask(text)
-    setText('')
+    if (text.trim()) {
+      addTask(text.trim())
+      setText('')
+    } else {
+      setError('Title is required')
+    }
   }
 
   const onPressHandler = (event: KeyboardEvent<HTMLInputElement>) => {
@@ -40,6 +46,7 @@ const ToDoList: React.FC<ToDoListPropsType> = ({
   }
 
   const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setError('')
     const currentValue = event.currentTarget.value
     setText(currentValue)
   }
@@ -82,13 +89,14 @@ const ToDoList: React.FC<ToDoListPropsType> = ({
       <div>
         <h3>{title}</h3>
         <div>
-          <input
+          <input className={error ? style.error : ''}
             value={text}
             onChange={onChangeHandler}
             onKeyDown={onPressHandler}
           />
           <button onClick={anotherTask}>+</button>
         </div>
+        {error && <div className={style.errorMessage}>{error}</div>}
         <ul>
           {tasksJSX}
         </ul>
