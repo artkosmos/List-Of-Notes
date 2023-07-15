17fe48ff804effc40611b168b60be44b1d0d62ce
Artem Kosmatkov <artemkosmatkov1994@gmail.com>
2023-07-15 21:02:48 +0300

    add storybook's story for Task.tsx

---
+++ src/Task.stories.tsx
@@ -0,0 +1,31 @@
+import type {Meta, StoryObj} from '@storybook/react';
+import {action} from '@storybook/addon-actions'
+import {Task} from './Task'
+
+const meta: Meta = {
+  title: 'TODOLIST/Task',
+  component: Task,
+  tags: ['autodocs'],
+  argTypes: {},
+  args: {},  // можно задавать начальные аргументы для всех историй
+}
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const TaskIsDoneStory: Story = {
+  args: {
+    task: {id: 1, title: 'JavaScript', isDone: true},
+    changeStatus: action('change status of checkbox'),
+    removeTask: action('delete task'),
+    updateTask: action('change title of task')
+  },
+}
+
+export const TaskIsNotDoneStory: Story = {
+  args: {
+    ...TaskIsDoneStory.args,
+    task: {id: 1, title: 'HTML/CSS', isDone: false},
+  },
+}
+
