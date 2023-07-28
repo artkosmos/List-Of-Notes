05a27ecaa74ae0a96a104fdd731f4c6842568fac
Artem Kosmatkov <artemkosmatkov1994@gmail.com>
2023-07-28 16:11:09 +0300

    add thunk-redux library and dispatch thunk typing

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,7 @@
-import {combineReducers, legacy_createStore} from "redux";
+import {AnyAction, applyMiddleware, combineReducers, legacy_createStore} from "redux";
 import {TodoListReducer} from "../reducers/todolists-reducer";
 import {TaskReducer} from "../reducers/tasks-reducer";
+import thunk, {ThunkDispatch} from "redux-thunk";
 
 export type StateType = ReturnType<typeof rootReducer>
 
@@ -9,4 +10,6 @@ const rootReducer = combineReducers({
   tasks: TaskReducer
 })
 
-export const store = legacy_createStore(rootReducer)
+export const store = legacy_createStore(rootReducer, applyMiddleware(thunk))
+
+export type AppDispatchType = ThunkDispatch<StateType, unknown, AnyAction>
