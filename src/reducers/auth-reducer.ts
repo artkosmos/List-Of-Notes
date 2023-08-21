1624999a943fca7aa8cf5d08e8934d991deea29c
Artem Kosmatkov <artemkosmatkov1994@gmail.com>
2023-08-21 14:10:31 +0300

    change reducer using RTK

--- src/reducers/auth-reducer.ts
+++ src/reducers/auth-reducer.ts
@@ -1,56 +1,37 @@
 import { Dispatch } from "redux"
-import { authAPI, ResponseType, ResultCodes } from "../api/todolist-api"
-import {
-  handleServerAppError,
-  handleServerNetworkError,
-} from "../utils/error-utils"
+import { authAPI, ResponseType, ResultCodes } from "api/todolist-api"
+import { handleServerAppError, handleServerNetworkError } from "utils/error-utils"
 import axios from "axios"
-import { FormType } from "../features/Login"
+import { FormType } from "features/Login"
 import { setIsInitializedAC, setPreloaderStatusAC } from "./app-reducer"
 import { cleanDataAC } from "./todolists-reducer"
-import { AppDispatchType } from "../store/store"
+import { AppDispatchType, AppThunk } from "store/store"
+import { createSlice, PayloadAction } from "@reduxjs/toolkit"
 
 type AuthStateType = {
   isLogin: boolean
   authUserLogin: null | string
 }
 
-type ActionType = SetIsLoginACType | SetAuthUserACType
+const slice = createSlice({
+  //подредюсер как раньше экшены и кейсы
+  name: "auth",
+  initialState: {
+    isLogin: false,
+    authUserLogin: null,
+  } as AuthStateType,
+  reducers: {
+    setIsLoggedIn: (state, action: PayloadAction<{ isLogin: boolean }>) => {
+      state.isLogin = action.payload.isLogin
+    },
+    setAuthUser: (state, action: PayloadAction<{ userLogin: string | null }>) => {
+      state.authUserLogin = action.payload.userLogin
+    },
+  },
+})
 
-const initialState = {
-  isLogin: false,
-  authUserLogin: null,
-}
-
-export const authReducer = (
-  state: AuthStateType = initialState,
-  action: ActionType,
-): AuthStateType => {
-  switch (action.type) {
-    case "SET-IS-LOGIN":
-      return { ...state, isLogin: action.value }
-    case "SET-AUTH-USER":
-      return { ...state, authUserLogin: action.userLogin }
-    default:
-      return state
-  }
-}
-
-type SetIsLoginACType = ReturnType<typeof setIsLogInAC>
-export const setIsLogInAC = (value: boolean) => {
-  return {
-    type: "SET-IS-LOGIN",
-    value,
-  } as const
-}
-
-type SetAuthUserACType = ReturnType<typeof setAuthUserAC>
-export const setAuthUserAC = (userLogin: string | null) => {
-  return {
-    type: "SET-AUTH-USER",
-    userLogin,
-  } as const
-}
+export const authAction = slice.actions
+export const authReducer = slice.reducer //общий редюсер
 
 export const checkIsLogInTC = () => async (dispatch: Dispatch) => {
   try {
@@ -58,14 +39,12 @@ export const checkIsLogInTC = () => async (dispatch: Dispatch) => {
     if (response.data.resultCode !== ResultCodes.OK) {
       handleServerAppError(response.data, dispatch)
     } else {
-      dispatch(setIsLogInAC(true))
-      dispatch(setAuthUserAC(response.data.data.login))
+      dispatch(authAction.setIsLoggedIn({ isLogin: true }))
+      dispatch(authAction.setAuthUser({ userLogin: response.data.data.login }))
     }
   } catch (error) {
     if (axios.isAxiosError<ResponseType>(error)) {
-      const errorMessage = error.response
-        ? error.response.data.messages[0]
-        : error.message
+      const errorMessage = error.response ? error.response.data.messages[0] : error.message
       handleServerNetworkError(errorMessage, dispatch)
     } else {
       const jsError = "Code compilation error"
@@ -77,20 +56,19 @@ export const checkIsLogInTC = () => async (dispatch: Dispatch) => {
 }
 
 export const logInTC =
-  (data: FormType) => async (dispatch: AppDispatchType) => {
+  (data: FormType): AppThunk =>
+  async (dispatch) => {
     dispatch(setPreloaderStatusAC("loading"))
     try {
       const response = await authAPI.logIn(data)
       if (response.data.resultCode !== ResultCodes.OK) {
         handleServerAppError(response.data, dispatch)
       } else {
-        dispatch(checkIsLogInTC())
+        dispatch(checkIsLogInTC()).then()
       }
     } catch (error) {
       if (axios.isAxiosError<ResponseType>(error)) {
-        const errorMessage = error.response
-          ? error.response.data.messages[0]
-          : error.message
+        const errorMessage = error.response ? error.response.data.messages[0] : error.message
         handleServerNetworkError(errorMessage, dispatch)
       } else {
         const jsError = "Code compilation error"
@@ -101,22 +79,20 @@ export const logInTC =
     }
   }
 
-export const logOutTC = () => async (dispatch: Dispatch) => {
+export const logOutTC = (): AppThunk => async (dispatch) => {
   dispatch(setPreloaderStatusAC("loading"))
   try {
     const response = await authAPI.logOut()
     if (response.data.resultCode !== ResultCodes.OK) {
       handleServerAppError(response.data, dispatch)
     } else {
-      dispatch(setIsLogInAC(false))
-      dispatch(setAuthUserAC(null))
+      dispatch(authAction.setIsLoggedIn({ isLogin: false }))
+      dispatch(authAction.setAuthUser({ userLogin: null }))
       dispatch(cleanDataAC())
     }
   } catch (error) {
     if (axios.isAxiosError<ResponseType>(error)) {
-      const errorMessage = error.response
-        ? error.response.data.messages[0]
-        : error.message
+      const errorMessage = error.response ? error.response.data.messages[0] : error.message
       handleServerNetworkError(errorMessage, dispatch)
     } else {
       const jsError = "Code compilation error"
