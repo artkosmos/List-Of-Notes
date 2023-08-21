50bcdc77cfa4c5c0abf35f4ae0305cd0f5febfcf
Artem Kosmatkov <artemkosmatkov1994@gmail.com>
2023-08-21 14:10:02 +0300

    recreate store using RTK

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,17 +1,13 @@
-import {
-  AnyAction,
-  applyMiddleware,
-  combineReducers,
-  legacy_createStore,
-} from "redux"
-import { TodoListReducer } from "../reducers/todolists-reducer"
-import { TaskReducer } from "../reducers/tasks-reducer"
-import thunk, { ThunkDispatch } from "redux-thunk"
-import { appReducer } from "../reducers/app-reducer"
+import { AnyAction, combineReducers } from "redux"
+import { TodoListReducer } from "reducers/todolists-reducer"
+import { TaskReducer } from "reducers/tasks-reducer"
+import { ThunkAction, ThunkDispatch } from "redux-thunk"
+import { appReducer } from "reducers/app-reducer"
 import { TypedUseSelectorHook, useSelector } from "react-redux"
-import { authReducer } from "../reducers/auth-reducer"
+import { authReducer } from "reducers/auth-reducer"
+import { configureStore } from "@reduxjs/toolkit"
 
-export type StateType = ReturnType<typeof rootReducer>
+export type StateType = ReturnType<typeof store.getState>
 
 const rootReducer = combineReducers({
   todolists: TodoListReducer,
@@ -20,10 +16,11 @@ const rootReducer = combineReducers({
   auth: authReducer,
 })
 
-export const store = legacy_createStore(rootReducer, applyMiddleware(thunk))
+export const store = configureStore({ reducer: rootReducer })
 
 // @ts-ignore
 window.store = store
 
 export type AppDispatchType = ThunkDispatch<StateType, unknown, AnyAction>
+export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, StateType, unknown, AnyAction>
 export const useAppSelector: TypedUseSelectorHook<StateType> = useSelector
