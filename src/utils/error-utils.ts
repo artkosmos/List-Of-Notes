1061fd84242021129299145c4eaa886a6a7e2d31
Artem Kosmatkov <artemkosmatkov1994@gmail.com>
2023-08-25 22:09:42 +0300

    updated custom error handler to get more sorts of errors

--- src/utils/error-utils.ts
+++ src/utils/error-utils.ts
@@ -1,20 +1,30 @@
-import { Dispatch } from "redux"
-import { ResponseType } from "api/todolist-api"
-import { appAction } from "reducers/app-reducer"
+import { Dispatch } from 'redux'
+import { appAction } from 'reducers/app-reducer'
+import axios from 'axios'
+import { ResponseType } from 'api/todolist-api'
+import { AppDispatchType } from 'store/store'
 
-// generic function
 export const handleServerAppError = <T>(data: ResponseType<T>, dispatch: Dispatch) => {
   if (data.messages.length) {
     dispatch(appAction.setError({ error: data.messages[0] }))
   } else {
-    dispatch(appAction.setError({ error: "Unknown error :(" }))
+    dispatch(appAction.setError({ error: 'Unknown error :(' }))
   }
-  dispatch(appAction.setPreloaderStatus({ status: "failed" }))
+  dispatch(appAction.setPreloaderStatus({ status: 'failed' }))
 }
 
-export const handleServerNetworkError = (errorMessage: string, dispatch: Dispatch) => {
-  dispatch(appAction.setError({ error: errorMessage }))
-  dispatch(appAction.setPreloaderStatus({ status: "failed" }))
+export const handleServerNetworkError = (error: unknown, dispatch: AppDispatchType) => {
+  if (axios.isAxiosError<ResponseType>(error)) {
+    const errorMessage = error.response ? error.response.data.messages[0] : error.message
+    dispatch(appAction.setError({ error: errorMessage }))
+  } else if (error instanceof Error) {
+    const errorMessage = `Native error: ${error.message}`
+    dispatch(appAction.setError({ error: errorMessage }))
+  } else {
+    const errorMessage = JSON.stringify(error)
+    dispatch(appAction.setError({ error: errorMessage }))
+  }
+  dispatch(appAction.setPreloaderStatus({ status: 'failed' }))
 }
 
 // type ErrorUtilsDispatchType = Dispatch<SetAppErrorActionType | SetAppStatusActionType>
