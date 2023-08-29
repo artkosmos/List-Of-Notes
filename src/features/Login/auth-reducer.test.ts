e5693e26a4ae3b761cf73d8ac60030aa76d7f72e
Artem Kosmatkov <artemkosmatkov1994@gmail.com>
2023-08-29 18:00:10 +0300

    fix tests after changing auth thunks using RTK

--- src/features/Login/auth-reducer.test.ts
+++ src/features/Login/auth-reducer.test.ts
@@ -1,30 +1,31 @@
-import { authAction, authReducer, AuthReducerState } from 'features/Login/auth-reducer'
+import { authReducer, AuthReducerState, authThunk } from 'features/Login/auth-reducer'
 
 let authState: AuthReducerState
 
-beforeEach(() => {
+test('should update state when authorization is successful', () => {
   authState = {
-    isLogin: false,
+    isAuth: false,
     authUserLogin: null,
   }
-})
 
-test('should update the status correctly', () => {
-  const isLogin = true
-
-  const resultState = authReducer(authState, authAction.setIsLoggedIn({ isLogin }))
-
-  expect(resultState.isLogin).toBeTruthy()
-  expect(resultState.authUserLogin).toBeNull()
-})
-
-test('should update the status of initialization', () => {
   const userLogin = 'jack-27'
+
+  const resultState = authReducer(authState, authThunk.checkIsAuth.fulfilled({ userLogin }, 'requiredId', {}))
+
+  expect(resultState.isAuth).toBeTruthy()
+  expect(resultState.authUserLogin).toBe(userLogin)
+})
+
+test('should update state correctly when log out', () => {
+  authState = {
+    isAuth: true,
+    authUserLogin: 'jonny-654',
+  }
+
   const noUserLogin = null
 
-  const resultState1 = authReducer(authState, authAction.setAuthUser({ userLogin }))
-  const resultState2 = authReducer(authState, authAction.setAuthUser({ userLogin: noUserLogin }))
+  const resultState = authReducer(authState, authThunk.logOut.fulfilled({ userLogin: noUserLogin }, 'requiredId', {}))
 
-  expect(resultState1.authUserLogin).toBe(userLogin)
-  expect(resultState2.authUserLogin).toBeNull()
+  expect(resultState.authUserLogin).toBeNull()
+  expect(resultState.isAuth).toBeFalsy()
 })
