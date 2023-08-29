e49288fa5078961a44fbce566be53b8045882a0b
Artem Kosmatkov <artemkosmatkov1994@gmail.com>
2023-08-29 17:59:31 +0300

    implement auth reducer thunks using RTK

--- src/features/Login/auth-reducer.ts
+++ src/features/Login/auth-reducer.ts
@@ -1,102 +1,93 @@
-import { Dispatch } from 'redux'
 import { handleServerNetworkError } from 'common/utils/handleNetworkAppError'
-import axios from 'axios'
-import { FormType } from 'features/Login/Login'
-import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit'
 import { appAction } from 'app/app-reducer'
 import { todolistsAction } from 'features/ListOfTodolists/todolists-reducer'
 import { authAPI } from 'features/Login/auth-api'
-import { BaseResponseType, ResultCodes } from 'common/types/api_types'
-import { handleServerAppError } from 'common/utils'
-import { AppThunk, AuthStateType } from 'common/types/app-types'
+import { FormType, ResultCodes } from 'common/types/api_types'
+import { createAppAsyncThunk, handleServerAppError } from 'common/utils'
+import { AuthStateType } from 'common/types/app-types'
 
 const slice = createSlice({
   name: 'auth',
   initialState: {
-    isLogin: false,
+    isAuth: false,
     authUserLogin: null,
   } as AuthStateType,
-  reducers: {
-    setIsLoggedIn: (state, action: PayloadAction<{ isLogin: boolean }>) => {
-      state.isLogin = action.payload.isLogin
-    },
-    setAuthUser: (state, action: PayloadAction<{ userLogin: string | null }>) => {
-      state.authUserLogin = action.payload.userLogin
-    },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(authThunk.checkIsAuth.fulfilled, (state, { payload }) => {
+        state.isAuth = true
+        state.authUserLogin = payload.userLogin
+      })
+      .addCase(authThunk.logOut.fulfilled, (state, { payload }) => {
+        state.isAuth = false
+        state.authUserLogin = payload.userLogin
+      })
   },
 })
 
-export const authAction = slice.actions
-export const authReducer = slice.reducer
-
 export type AuthReducerState = ReturnType<typeof slice.getInitialState>
 
-export const checkIsLogInTC = () => async (dispatch: Dispatch) => {
+const checkIsAuth = createAppAsyncThunk<{ userLogin: string | null }, {}>('auth/checkIsAuth', async (_, thunkAPI) => {
+  const { dispatch, rejectWithValue } = thunkAPI
+
   try {
     const response = await authAPI.checkIsLogIn()
     if (response.data.resultCode !== ResultCodes.OK) {
       handleServerAppError(response.data, dispatch)
+      return rejectWithValue(null)
     } else {
-      dispatch(authAction.setIsLoggedIn({ isLogin: true }))
-      dispatch(authAction.setAuthUser({ userLogin: response.data.data.login }))
+      return { userLogin: response.data.data.login }
     }
   } catch (error) {
-    if (axios.isAxiosError<BaseResponseType>(error)) {
-      const errorMessage = error.response ? error.response.data.messages[0] : error.message
-      handleServerNetworkError(errorMessage, dispatch)
-    } else {
-      const jsError = 'Code compilation error'
-      handleServerNetworkError(jsError, dispatch)
-    }
+    handleServerNetworkError(error, dispatch)
+    return rejectWithValue(null)
   } finally {
     dispatch(appAction.setIsInitialized({ isInitialized: true }))
   }
-}
+})
 
-export const logInTC =
-  (data: FormType): AppThunk =>
-  async (dispatch) => {
-    dispatch(appAction.setPreloaderStatus({ status: 'loading' }))
-    try {
-      const response = await authAPI.logIn(data)
-      if (response.data.resultCode !== ResultCodes.OK) {
-        handleServerAppError(response.data, dispatch)
-      } else {
-        dispatch(checkIsLogInTC()).then()
-      }
-    } catch (error) {
-      if (axios.isAxiosError<BaseResponseType>(error)) {
-        const errorMessage = error.response ? error.response.data.messages[0] : error.message
-        handleServerNetworkError(errorMessage, dispatch)
-      } else {
-        const jsError = 'Code compilation error'
-        handleServerNetworkError(jsError, dispatch)
-      }
-    } finally {
-      dispatch(appAction.setPreloaderStatus({ status: 'succeeded' }))
-    }
-  }
+const logIn = createAppAsyncThunk<any, FormType>('auth/logIn', async (data, thunkAPI) => {
+  const { dispatch, rejectWithValue } = thunkAPI
 
-export const logOutTC = (): AppThunk => async (dispatch) => {
   dispatch(appAction.setPreloaderStatus({ status: 'loading' }))
+  try {
+    const response = await authAPI.logIn(data)
+    if (response.data.resultCode !== ResultCodes.OK) {
+      handleServerAppError(response.data, dispatch)
+      return rejectWithValue(null)
+    } else {
+      dispatch(authThunk.checkIsAuth({}))
+    }
+  } catch (error) {
+    handleServerNetworkError(error, dispatch)
+    return rejectWithValue(null)
+  } finally {
+    dispatch(appAction.setPreloaderStatus({ status: 'idle' }))
+  }
+})
+
+const logOut = createAppAsyncThunk<{ userLogin: string | null }, {}>('auth/logOut', async (_, thunkAPI) => {
+  const { dispatch, rejectWithValue } = thunkAPI
+
   try {
     const response = await authAPI.logOut()
     if (response.data.resultCode !== ResultCodes.OK) {
       handleServerAppError(response.data, dispatch)
+      return rejectWithValue(null)
     } else {
-      dispatch(authAction.setIsLoggedIn({ isLogin: false }))
-      dispatch(authAction.setAuthUser({ userLogin: null }))
       dispatch(todolistsAction.cleanStateData())
+      return { userLogin: null }
     }
   } catch (error) {
-    if (axios.isAxiosError<BaseResponseType>(error)) {
-      const errorMessage = error.response ? error.response.data.messages[0] : error.message
-      handleServerNetworkError(errorMessage, dispatch)
-    } else {
-      const jsError = 'Code compilation error'
-      handleServerNetworkError(jsError, dispatch)
-    }
+    handleServerNetworkError(error, dispatch)
+    return rejectWithValue(null)
   } finally {
-    dispatch(appAction.setPreloaderStatus({ status: 'succeeded' }))
+    dispatch(appAction.setPreloaderStatus({ status: 'idle' }))
   }
-}
+})
+
+export const authAction = slice.actions
+export const authReducer = slice.reducer
+export const authThunk = { checkIsAuth, logOut, logIn }
