12a51156a3aa53825a198d2003450d2c8af7aeaa
Artem Kosmatkov <artemkosmatkov1994@gmail.com>
2023-09-11 15:29:45 +0300

    implement decomposition todolist title elements into a separate component

--- src/features/ListOfTodolists/ui/TodolistRedux/ToDoListRedux.tsx
+++ src/features/ListOfTodolists/ui/TodolistRedux/ToDoListRedux.tsx
@@ -1,14 +1,12 @@
 import style from 'features/ListOfTodolists/ui/TodolistRedux/ToDoList.module.css'
-import IconButton from '@mui/material/IconButton'
-import DeleteIcon from '@mui/icons-material/Delete'
 import { useDispatch } from 'react-redux'
 import { tasksThunk } from 'features/ListOfTodolists/model/tasks-reducer'
-import { todolistsThunk } from 'features/ListOfTodolists/model/todolists-reducer'
 import { useCallback } from 'react'
-import { AddItemForm, EditableSpan } from 'common/components'
+import { AddItemForm } from 'common/components'
 import { AppDispatchType, AppTodolistType } from 'common/types/app-types'
 import { TaskFilterButtons } from 'features/ListOfTodolists/ui/TodolistRedux/TaskFilterButtons/TaskFilterButtons'
 import { TasksList } from 'features/ListOfTodolists/ui/TodolistRedux/Tasks/TasksList'
+import { TodoTitle } from 'features/ListOfTodolists/ui/TodolistRedux/TodoTitle/TodoTitle'
 
 type ToDoListProps = {
   todolist: AppTodolistType
@@ -19,33 +17,14 @@ export const ToDoListRedux = ({ todolist }: ToDoListProps) => {
 
   const dispatch = useDispatch<AppDispatchType>()
 
-  const removeToDoListHandler = useCallback(() => {
-    dispatch(todolistsThunk.deleteTodolist(id))
-  }, [])
-
   const addTaskHandler = useCallback((title: string) => {
     dispatch(tasksThunk.addTask({ todolistId: id, title }))
-  }, [])
-
-  const updateTodolistTitle = useCallback((title: string) => {
-    dispatch(todolistsThunk.updateTodoTitle({ todolistId: id, title }))
   }, [])
 
   return (
     <div>
       <div className={style.todolist}>
-        <IconButton
-          aria-label="delete"
-          onClick={removeToDoListHandler}
-          className={style.delete}
-          disabled={entityStatus === 'loading'}
-        >
-          <DeleteIcon />
-          <span className={style.deleteText}>Delete list</span>
-        </IconButton>
-        <h2>
-          <EditableSpan oldTitle={title} callBack={updateTodolistTitle} disabled={entityStatus === 'loading'} />
-        </h2>
+        <TodoTitle id={id} title={title} entityStatus={entityStatus} />
         <AddItemForm callBack={addTaskHandler} disabled={entityStatus === 'loading'} />
         <TasksList id={id} filter={filter} />
         <div className={style.buttonWrapper}>
---
+++ src/features/ListOfTodolists/ui/TodolistRedux/TodoTitle/TodoTitle.tsx
@@ -0,0 +1,43 @@
+import React, { useCallback } from 'react'
+import IconButton from '@mui/material/IconButton'
+import style from 'features/ListOfTodolists/ui/TodolistRedux/ToDoList.module.css'
+import DeleteIcon from '@mui/icons-material/Delete'
+import { EditableSpan } from 'common/components'
+import { todolistsThunk } from 'features/ListOfTodolists/model/todolists-reducer'
+import { useDispatch } from 'react-redux'
+import { AppDispatchType, RequestStatusType } from 'common/types/app-types'
+
+type TodoTitleProps = {
+  id: string
+  title: string
+  entityStatus: RequestStatusType
+}
+
+export const TodoTitle = ({ id, title, entityStatus }: TodoTitleProps) => {
+  const dispatch = useDispatch<AppDispatchType>()
+
+  const removeToDoListHandler = () => {
+    dispatch(todolistsThunk.deleteTodolist(id))
+  }
+
+  const updateTodolistTitle = useCallback((title: string) => {
+    dispatch(todolistsThunk.updateTodoTitle({ todolistId: id, title }))
+  }, [])
+
+  return (
+    <>
+      <IconButton
+        aria-label="delete"
+        onClick={removeToDoListHandler}
+        className={style.delete}
+        disabled={entityStatus === 'loading'}
+      >
+        <DeleteIcon />
+        <span className={style.deleteText}>Delete list</span>
+      </IconButton>
+      <h2>
+        <EditableSpan oldTitle={title} callBack={updateTodolistTitle} disabled={entityStatus === 'loading'} />
+      </h2>
+    </>
+  )
+}
