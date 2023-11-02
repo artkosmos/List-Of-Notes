1ccfa80a631a6d53c701ac6dfbf2bb362f56bcb9
Artem Kosmatkov <artemkosmatkov1994@gmail.com>
2023-11-02 14:45:31 +0300

    header refactor

--- src/common/components/ButtonAppBar/ButtonAppBar.tsx
+++ src/common/components/ButtonAppBar/ButtonAppBar.tsx
@@ -4,8 +4,6 @@ import Box from '@mui/material/Box'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button'
-import IconButton from '@mui/material/IconButton'
-import MenuIcon from '@mui/icons-material/Menu'
 import { useDispatch } from 'react-redux'
 import { authLoginSelector, isLoginSelector } from 'app/app-selectors'
 import { AppDispatchType } from 'common/types/app-types'
@@ -27,11 +25,8 @@ export function ButtonAppBar() {
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+      <AppBar position="static" sx={{ backgroundColor: 'rgba(0,23,40,0.45)' }}>
         <Toolbar>
-          <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1, marginLeft: '30px' }}>
             {loginToShow}
           </Typography>
